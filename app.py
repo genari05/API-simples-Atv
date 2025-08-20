48f47a160e032f2190d1a16f60a520da6802e319
Mathx8 <matheusqueirozdev@gmail.com>
2025-08-20 12:12:18 -0300

    put e delete ok

--- app.py
+++ app.py
@@ -13,12 +13,10 @@ convidados = [
 def bem_vindo():
     return 'Faculdade Impacta Tecnologia', 200
 
-# Lista todos os convidados
 @app.route('/convidados', methods=['GET'])
 def lista_convidados():
     return jsonify(convidados), 200
 
-# Busca convidado pelo id
 @app.route('/convidados/<int:id>', methods=['GET'])
 def id_por_convidado(id):
     for convidado in convidados:
@@ -27,28 +25,23 @@ def id_por_convidado(id):
     return jsonify({'mensagem': 'Convidado não encontrado'}), 404
 
 
-# Adiciona novo convidado
-@app.route('/adicionar', methods=['POST'])
+@app.route('/convidados', methods=['POST'])
 def adicionar_convidado():
     dados = request.get_json()
 
     nome = dados.get('nome', '')
     email = dados.get('email', '')
 
-    # Função para validar e-mail
     def validar_email(email):
         padrao = r'^[\w\.-]+@[\w\.-]+\.\w+$'
         return re.match(padrao, email) is not None
 
-    # Checa se nome e email foram enviados
     if not nome or not email:
         return jsonify({'erro': 'Nome e e-mail são obrigatórios'}), 400
 
-    # Valida formato do e-mail
     if not validar_email(email):
         return jsonify({'erro': 'E-mail inválido'}), 400
 
-    # Gera novo ID automaticamente
     novo_id = len(convidados) + 1
     novo_convidado = {
         'id': novo_id,
@@ -59,6 +52,38 @@ def adicionar_convidado():
     convidados.append(novo_convidado)
     return jsonify(novo_convidado), 201
 
+@app.route('/convidados/<int:id>', methods=['PUT'])
+def atualizar_convidado(id):
+    dados = request.get_json()
+
+    nome = dados.get('nome', '')
+    email = dados.get('email', '')
+
+    def validar_email(email):
+        padrao = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+        return re.match(padrao, email) is not None
+
+    if not nome or not email:
+        return jsonify({'erro': 'Nome e e-mail são obrigatórios'}), 400
+
+    if not validar_email(email):
+        return jsonify({'erro': 'E-mail inválido'}), 400
+
+    for convidado in convidados:
+        if convidado['id'] == id:
+            convidado['nome'] = nome
+            convidado['email'] = email
+            return jsonify(convidado), 200
+
+    return jsonify({'mensagem': 'Convidado não encontrado'}), 404
+
+@app.route('/convidados/<int:id>', methods=['DELETE'])
+def remover_convidado(id):
+    for convidado in convidados:
+        if convidado['id'] == id:
+            convidados.remove(convidado)
+            return jsonify({'mensagem': 'Convidado removido'})
+    return jsonify({'mensagem': 'Convidado não encontrado'}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
